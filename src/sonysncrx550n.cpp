eaab95a266bfbaf796aa14fe91b7c686ce7c892e
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-01-31 01:31:18 +0100

    Add documentation and finish to implement the driver

--- src/sonysncrx550n.cpp
+++ src/sonysncrx550n.cpp
@@ -4,66 +4,101 @@
 #include <QNetworkRequest>
 #include <QDateTime>
 #include <QFile>
+#include <QStringList>
 
 // stl library
 #include <iostream>
+
+// Initialise the value that the zoom position can take
+const std::map< QString, QString > SonySNCRX550N::zoom_position_key_to_hex = { {"oz-1", "0000"}, {"oz-2", "1760"}, {"oz-3", "214C"}, {"oz-4", "2722"}, {"oz-5", "2B22"}, {"oz-6", "2E20"}, {"oz-7", "3080"}, {"oz-8", "3278"}, {"oz-9", "3426"}, {"oz-10", "359E"}, {"oz-11", "36EE"}, {"oz-12", "381C"}, {"oz-13", "392E"}, {"oz-14", "3A26"}, {"oz-15", "3B08"}, {"oz-16", "3BD4"}, {"oz-17", "3C8C"}, {"oz-18", "3D2E"}, {"oz-19", "3DBC"}, {"oz-20", "3E58"}, {"oz-21", "3EA2"}, {"oz-22", "3F00"}, {"oz-23", "3F4E"}, {"oz-24", "3F92"}, {"oz25", "3FCC"}, {"dz-1", "4000"}, {"dz-2", "6000"}, {"dz-3", "6A80"}, {"dz-4", "7000"}, {"dz-5", "7300"}, {"dz-6", "7540"}, {"dz-7", "76C0"}, {"dz-8", "7800"}, {"dz-9", "78C0"}, {"dz-10", "7980"}, {"dz-11", "7A00"}, {"dz-12", "7AC0"} };
+const std::map< QString, QString > SonySNCRX550N::zoom_position_hex_to_key = { {"0000", "oz-1"}, {"1760", "oz-2"}, {"214C", "oz-3"}, {"2722", "oz-4"}, {"2B22", "oz-5"}, {"2E20", "oz-6"}, {"3080", "oz-7"}, {"3278", "oz-8"}, {"3426", "oz-9"}, {"359E", "oz-10"}, {"36EE", "oz-11"}, {"381C", "oz-12"}, {"392E", "oz-13"}, {"3A26", "oz-14"}, {"3B08", "oz-15"}, {"3BD4", "oz-16"}, {"3C8C", "oz-17"}, {"3D2E", "oz-18"}, {"3DBC", "oz-19"}, {"3E58", "oz-20"}, {"3EA2", "oz-21"}, {"3F00", "oz-22"}, {"3F4E", "oz-23"}, {"3F92", "oz-24"}, {"3FCC", "oz25"}, {"4000", "dz-1"}, {"6000", "dz-2"}, {"6A80", "dz-3"}, {"7000", "dz-4"}, {"7300", "dz-5"}, {"7540", "dz-6"}, {"76C0", "dz-7"}, {"7800", "dz-8"}, {"78C0", "dz-9"}, {"7980", "dz-10"}, {"7A00", "dz-11"}, {"7AC0", "dz-12"} };
+
+const std::map< QString, QString > SonySNCRX550N::focus_position_key_to_hex = { {"f-inf", "1000"}, {"f-7200", "2000"}, {"f-3300", "3000"}, {"f-2000", "4000"}, {"f-1300", "5000"}, {"f-1000", "6000"}, {"f-800", "7000"}, {"f-400", "8000"}, {"f-200", "9000"}, {"f-110", "A000"}, {"f-60", "B000"}, {"f-35", "C000"} };
+const std::map< QString, QString > SonySNCRX550N::focus_position_hex_to_key = { {"1000", "f-inf"}, {"2000", "f-7200"}, {"3000", "f-3300"}, {"4000", "f-2000"}, {"5000", "f-1300"}, {"6000", "f-1000"}, {"7000", "f-800"}, {"8000", "f-400"}, {"9000", "f-200"}, {"A000", "f-110"}, {"B000", "f-60"}, {"C000", "f-35"} };
 
 SonySNCRX550N::SonySNCRX550N(const QString& _ip_address, QObject *parent) : 
   QObject(parent) {
   // Set up the ip address
   set_ip_address(_ip_address);
-  absolute_motion(0,0,24);
+  // Set the original positions and speed
+  pan_pos = 0;
+  tilt_pos = 0;
+  speed = 24;
+  zoom_pos = "oz-1";
+  focus_pos = "f-1300";
+  // Move to the position
+  absolute_motion(pan_pos, tilt_pos, speed);
+  absolute_zoom(zoom_pos);
 }
 
 // Set the IP address
 void SonySNCRX550N::set_ip_address(const QString& _ip_address) {
   ip_address = _ip_address;
   // Create url for future http request
-  url_request_rel_command.setUrl("http://" + ip_address + "/command/ptzf.cgi?");
-  url_request_abs_command.setUrl("http://" + ip_address + "/command/ptzf.cgi?");
+  url_request_command.setUrl("http://" + ip_address + "/command/ptzf.cgi?");
   url_request_one_shot.setUrl("http://" + ip_address + "/oneshotimage.jpg");
   // Initialise the network access manager
   net_acc_manager = new QNetworkAccessManager(this);
   // Connect some signal-slot in order to be able to save the data
-  // Catch the signal when the data are ready to be stored
   connect(net_acc_manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(net_data_transmitted(QNetworkReply*)));
 }
 
 // Relative motion - The different parameters are given as:
 // _p_angle: pan position  | -360 to 360
-// _t_angle: tilt position |  -90 to 90
+// _t_angle: tilt position |  -96 to 96
 // speed: engine speed     |    1 to 24
 void SonySNCRX550N::relative_motion(const long _p_angle, const long _t_angle, const long speed) {
+  // Check the speed
+  if ((speed < low_speed) || (speed > high_speed)) {
+    std::cout << "The speed set is out of range !!! Stopping action now!!!" << std::endl;
+    return;
+  }
   QString query = convert_panning_rel_to_hex(_p_angle) + "," + convert_tilt_rel_to_hex(_t_angle) + "," + QString::number(speed);
-  url_request_rel_command.addQueryItem("relativepantilt", query);
+  url_request_command.addQueryItem("relativepantilt", query);
   // Send the request
-  network_request(url_request_rel_command);
+  network_request(url_request_command);
 }
 
 // Absolute motion - The different parameters are given as:
 // _p_angle: pan position  | -180 to 180
-// _t_angle: tilt position |  -45 to 45
+// _t_angle: tilt position |  -48 to 48
 // speed: engine speed     |    1 to 24
 void SonySNCRX550N::absolute_motion(const long _p_angle, const long _t_angle, const long speed) {
+  // Check the speed
+  if ((speed < low_speed) || (speed > high_speed)) {
+    std::cout << "The speed set is out of range !!! Stopping action now!!!" << std::endl;
+    return;
+  }
   QString query = convert_panning_abs_to_hex(_p_angle) + "," + convert_tilt_abs_to_hex(_t_angle) + "," + QString::number(speed);
-  url_request_abs_command.addQueryItem("absolutepantilt", query);
+  url_request_command.addQueryItem("absolutepantilt", query);
   // Send the request
-  network_request(url_request_abs_command);
+  network_request(url_request_command);
 }
 
-// TO CHECK -------------------------------------------------
-// ONLY ONE REPLY POINTER IS ALLOCATED. IT IS MAYBE NEEDED TO 
-// MAKE A VECTOR OF POINTER IN ORDER TO GET SEVERAL REPLIES
-// SINCE THAT MULTIPLES MESSAGES CAN BE SEND AT THE SAME TIME
-// CHECKED -------------------------------------------------- 
+// Private function in order to control the zoom
+void SonySNCRX550N::absolute_zoom(const QString& _zoom_position) {
+  // Convert the zoom to hexadecimal code
+  auto hexa_zoom = zoom_position_key_to_hex.find(_zoom_position);
+  url_request_command.addQueryItem("absolutezoom", hexa_zoom->second);
+  // Send the request
+  network_request(url_request_command);
+}
+
+// Private function in order to control the focus
+void SonySNCRX550N::absolute_focus(const QString& _focus_position) {
+  url_request_command.addQueryItem("absolutefocus", _focus_position);
+  // Send the request
+  network_request(url_request_command);
+}
+
+// Store an image
+void SonySNCRX550N::grab_image() {
+  network_request(url_request_one_shot);
+}
 
 void SonySNCRX550N::network_request(const QUrl& _url) {
   // Get the reply for the url request
   net_acc_manager->get(QNetworkRequest(_url));
-}
-
-void SonySNCRX550N::grab_image() {
-  network_request(url_request_one_shot);
 }
 
 // slot to take decision about the transmitted data
@@ -84,144 +119,50 @@ void SonySNCRX550N::net_data_transmitted(QNetworkReply* _p_net_reply) {
     // Close the file
     file.close();
   }
-  else if (_p_net_reply->url().toString().contains(url_request_rel_command.toString())) {
+  else if (_p_net_reply->url().toString().contains("relativepantilt")) {
+    // Update the position since that the query was completed
+    // Get the query which was pending
+    QString params = url_request_command.queryItemValue("relativepantilt");
+    QStringList params_split = params.split(",", QString::SkipEmptyParts);
+    double rel_pan = convert_panning_to_deg(params_split.at(0));
+    double rel_tilt = convert_panning_to_deg(params_split.at(1));
+    bool ok;
+    // Update the position
+    speed = static_cast<long> (params_split.at(2).toInt(&ok, 10));
+    pan_pos = std::fmod((pan_pos + rel_pan), total_panning_angle);
+    tilt_pos += rel_tilt;
+    if (tilt_pos > max_tilt_abs)
+      tilt_pos = max_tilt_abs;
+    else if (tilt_pos < min_tilt_abs)
+      tilt_pos = min_tilt_abs;
     // Remove the url query from the list
-    url_request_rel_command.removeQueryItem("relativepantilt");
+    url_request_command.removeQueryItem("relativepantilt");
   }
-  else if (_p_net_reply->url().toString().contains(url_request_abs_command.toString())) {
+  else if (_p_net_reply->url().toString().contains("absolutepantilt")) {
+    // Update the position since that the query was completed
+    // Get the query which was pending
+    QString params = url_request_command.queryItemValue("absolutepantilt");
+    QStringList params_split = params.split(",", QString::SkipEmptyParts);
+    pan_pos = convert_panning_to_deg(params_split.at(0));
+    tilt_pos = convert_panning_to_deg(params_split.at(1));
+    bool ok;
+    // Update the position
+    speed = static_cast<long> (params_split.at(2).toInt(&ok, 10));
     // Remove the url query from the list
-    url_request_abs_command.removeQueryItem("absolutepantilt");
+    url_request_command.removeQueryItem("absolutepantilt");
+  }
+  else if (_p_net_reply->url().toString().contains("absolutezoom")) {
+    // Convert hexadecimal to key
+    auto key_zoom = zoom_position_hex_to_key.find(url_request_command.queryItemValue("absolutezoom"));
+    zoom_pos = key_zoom->second;
+    // Remove the url query from the list
+    url_request_command.removeQueryItem("absolutezoom");
+  }
+  else if (_p_net_reply->url().toString().contains("absolutefocus")) {
+    auto key_focus = focus_position_hex_to_key.find(url_request_command.queryItemValue("absolutefocus"));
+    focus_pos = key_focus->second;
+    // Remove the url query from the list
+    url_request_command.removeQueryItem("absolutefocus");
   }
   _p_net_reply->deleteLater();
 }
-
-// void SonyControl::startRequests(QUrl _url)
-// {
-//   qnam.get(QNetworkRequest(_url));
-// }
-
-// void SonyControl::startRequestDownload(QUrl _url)
-// {
-//   downloadFinished = false;
-//   reply = qnam.get(QNetworkRequest(_url));
-//   connect(reply, SIGNAL(finished()),
-// 	  this, SLOT(httpFinished()));
-//   connect(reply, SIGNAL(readyRead()),
-// 	  this, SLOT(httpReadyRead()));
-
-// }
-
-// void SonyControl::absoluteConf(int _apan, int _atilt, int _aspeed){
-//   url = URLCommand;
-//   QString a = convToHexPan(_apan) +","+convToHexTilt(_atilt)+","+QString("%1").arg(_aspeed);
-//   url.addQueryItem("absolutepantilt",a);
-//   startRequests(url);
-// }
-
-// //For relative movement
-// void SonyControl::relativeConf(int _rpan, int _rtilt, int _rspeed){
-//   url = URLCommand;
-//   QString a = convToHexPan(_rpan)+","+convToHexTilt(_rtilt)+","+QString("%1").arg(_rspeed);
-//   url.addQueryItem("relativepantilt",a);
-//   startRequests(url);
-// }
-
-
-// QString SonyControl::convToHexPan(int _decP){
-//   uint step=floor(_decP/0.022);
-//   char hash[4];
-//   uint x[4];
-
-//   //converting to hexa
-//   x[0] = step & 0xF000;
-//   x[0] = x[0]>>12;
-//   x[1] = step & 0x0F00;
-//   x[1] = x[1]>>8;
-//   x[2] = step & 0x00F0;
-//   x[2] = x[2]>>4;
-//   x[3] = step & 0x000F;
-
-//   hash[0]= SonyControl::hexVal[x[0]];
-//   hash[1]= SonyControl::hexVal[x[1]];
-//   hash[2]= SonyControl::hexVal[x[2]];
-//   hash[3]= SonyControl::hexVal[x[3]];
-
-//   QString p = QString(hash[0])+QString(hash[1])+QString(hash[2])+QString(hash[3]);
-//   return(p);
-// }
-
-// QString SonyControl::convToHexTilt(int _decT){
-//   uint step=floor(_decT/0.023);
-//   char hash[4];
-//   uint x[4];
-
-//   //converting to hexa
-//   x[0] = step & 0xF000;
-//   x[0] = x[0]>>12;
-//   x[1] = step & 0x0F00;
-//   x[1] = x[1]>>8;
-//   x[2] = step & 0x00F0;
-//   x[2] = x[2]>>4;
-//   x[3] = step & 0x000F;
-
-//   hash[0]= SonyControl::hexVal[x[0]];
-//   hash[1]= SonyControl::hexVal[x[1]];
-//   hash[2]= SonyControl::hexVal[x[2]];
-//   hash[3]= SonyControl::hexVal[x[3]];
-
-//   QString t = QString(hash[0]).append(QString(hash[1]).append(QString(hash[2]).append(QString(hash[3]))));
-//   return t;
-
-// }
-
-// void SonyControl::downloadFile(QString fname)
-// {
-//   url = URLDownload;
-
-//   //QFileInfo fileInfo(url.path());
-
-//   if (i>32){
-//     i=0;
-//   };
-//   i=i+1;
-//   fileName = fname+QString("%1").arg(i,2,'d',0,'0')+".png";
-//   file = new QFile(fileName);
-//   if (!file->open(QIODevice::WriteOnly)) {
-//     QMessageBox::information(0, tr("HTTP"),
-// 			     tr("Unable to save the file %1: %2.")
-// 			     .arg(fileName).arg(file->errorString()));
-//     delete file;
-//     file = 0;
-//     return;
-//   }
-//   startRequestDownload(url);
-// }
-
-// void SonyControl::httpFinished()
-// {
-
-//   file->flush();
-//   file->close();
-//   reply->deleteLater();
-//   reply = 0;
-//   delete file;
-//   file = 0;
-
-//   downloadFinished = true;
-// }
-
-// void SonyControl::httpReadyRead()
-// {
-//   // this slot gets called every time the QNetworkReply has new data.
-//   // We read all of its new data and write it into the file.
-//   // That way we use less RAM than when reading it at the finished()
-//   // signal of the QNetworkReply
-//   if (file)
-//     file->write(reply->readAll());
-// }
-
-// void SonyControl::delay(int _delay) {
-//   QTime dieTime= QTime::currentTime().addSecs(_delay);
-//   while( QTime::currentTime() < dieTime )
-//     QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
-// }
--- src/sonysncrx550n.h
+++ src/sonysncrx550n.h
@@ -30,7 +30,7 @@ public:
   /* Command management */
   // Relative motion - The different parameters are given as:
   // _p_angle: pan position  | -360 to 360
-  // _t_angle: tilt position |  -90 to 90
+  // _t_angle: tilt position |  -96 to 96
   // speed: engine speed     |    1 to 24
   void relative_motion(const long _p_angle = 0, const long _t_angle = 0, const long speed = 0);
   
@@ -41,8 +41,21 @@ public:
   void absolute_motion(const long _p_angle = 0, const long _t_angle = 0, const long speed = 0);
 
   /* Camera management */
+  // Private function in order to control the optical zoom
+  void absolute_zoom(const QString& _zoom_position = "oz-1");
+
+  // Private function in order to control the optical zoom
+  void absolute_focus(const QString& _focus_position = "f-200");
+
   // Private function to store an image
   void grab_image();
+
+  // Function to get the position of the camera
+  inline void get_camera_positions(double& _pan_pos, double& _tilt_pos, QString& _zoom_pos, QString& _focus_pos) const { _pan_pos = pan_pos; _tilt_pos = tilt_pos; _zoom_pos = zoom_pos; _focus_pos = focus_pos; }
+  inline double get_pan_position() const { return pan_pos; }
+  inline double get_tilt_position() const { return tilt_pos; }
+  inline QString get_zoom_position() const { return zoom_pos; }
+  inline QString get_focus_position() const { return focus_pos; }
 
   /* PRIVATE MEMBERS AND FUNCTIONS */
 
@@ -59,15 +72,18 @@ private:
 
   /* Command management */
   // Private member regarding engine command
-  QUrl url_request_abs_command;
-  QUrl url_request_rel_command;
+  QUrl url_request_command;
 
   // Private member for the position of the camera
   double pan_pos;
   double tilt_pos;
-  long zoom_opt;
-  long zoom_dig;
-  double focus;
+  QString zoom_pos;
+  QString focus_pos;
+
+  // Private members regarding the speed
+  long speed;
+  static const long low_speed = 1;
+  static const long high_speed = 24;
 
   // Private parameter regarding the panning
   static const long number_panning_steps = 16320;
@@ -77,8 +93,9 @@ private:
   static constexpr double min_panning_abs = -180.0;
   static constexpr double max_panning_abs = 180.0;
 
-  // RELATIVE CASE - Private function to convert an panning angle in degrees to hexadecimal
-  inline QString convert_panning_rel_to_hex(const double _p_angle) {
+  // RELATIVE CASE - PAN
+  // Degree to hexadecimal
+  inline QString convert_panning_rel_to_hex(const double _p_angle) const {
     if ((_p_angle < min_panning_rel)||(_p_angle > max_panning_rel)) {
       std::cout << "The pan angle requested is to small or to large!!! The camera will not move !!!" << std::endl;
       return QString::number(0, 16).toUpper();
@@ -86,17 +103,26 @@ private:
     return QString::number(static_cast<long> (std::round(_p_angle * static_cast<double> (number_panning_steps) / total_panning_angle)), 16).toUpper();
   }
 
-  // ABSOLUTE CASE - Private function to convert an panning angle in degrees to hexadecimal
-  inline QString convert_panning_abs_to_hex(const double _p_angle) {
+  // ABSOLUTE CASE - PAN
+  // Degree to hexadecimal
+  inline QString convert_panning_abs_to_hex(const double _p_angle) const {
     if ((_p_angle < min_panning_abs)||(_p_angle > max_panning_abs)) {
       std::cout << "The pan angle requested is to small or to large!!! The camera will not move !!!" << std::endl;
       return QString::number(0, 16).toUpper();
     }
     return QString::number(static_cast<long> (std::round(_p_angle * static_cast<double> (number_panning_steps) / total_panning_angle)), 16).toUpper();
   }
+  
+  // Hexadecimal to degree
+  inline double convert_panning_to_deg(const QString _p_hexa) const {
+    // Convert hexadecimal to decimal steps
+    bool ok;
+    int dec_steps = _p_hexa.toInt(&ok, 16);
+    // Convert the steps to degree
+    return dec_steps * total_panning_angle / static_cast<double> (number_panning_steps);
+  }
 
   // Private parameter regarding the tilting
-  //FROM THE DOCUMENTATION IT SEEMS THAT THE ENGINE CANNOT REACH SOME REMAINING 6 DEGREES
   static const long number_tilt_steps = 4352;
   static constexpr double total_tilt_angle = 96.0;
   static constexpr double min_tilt_rel = -96.0;
@@ -104,8 +130,9 @@ private:
   static constexpr double min_tilt_abs = -48.0;
   static constexpr double max_tilt_abs = 48.0;
 
-  // RELATIVE CASE - Private function to convert an panning angle in degrees to hexadecimal
-  inline QString convert_tilt_rel_to_hex(const double _t_angle) {
+  // RELATIVE CASE - TILT
+  // Degree to hexadecimal
+  inline QString convert_tilt_rel_to_hex(const double _t_angle) const {
     if ((_t_angle < min_tilt_rel)||(_t_angle > max_tilt_rel)) {
       std::cout << "The pan angle requested is to small or to large!!! The camera will not move !!!" << std::endl;
       return QString::number(0, 16).toUpper();
@@ -113,8 +140,9 @@ private:
     return QString::number(static_cast<long> (std::round(_t_angle * static_cast<double> (number_tilt_steps) / total_tilt_angle)), 16).toUpper();
   }
 
-  // ABSOLUTE CASE - Private function to convert an tilt angle in degrees to hexadecimal
-  inline QString convert_tilt_abs_to_hex(const double _t_angle) {
+  // ABSOLUTE CASE - TILT
+  // Degree to hexadecimal
+  inline QString convert_tilt_abs_to_hex(const double _t_angle) const {
     if ((_t_angle < min_tilt_abs)||(_t_angle > max_tilt_abs)) {
       std::cout << "The pan angle requested is to small or to large!!! The camera will not move !!!" << std::endl;
       return QString::number(0, 16).toUpper();
@@ -122,61 +150,38 @@ private:
     return QString::number(static_cast<long> (std::round(_t_angle * static_cast<double> (number_tilt_steps) / total_tilt_angle)), 16).toUpper();
   }
 
+  // Hexadecimal to degree
+  inline double convert_tilt_to_deg(const QString _p_hexa) const {
+    // Convert hexadecimal to decimal steps
+    bool ok;
+    int dec_steps = _p_hexa.toInt(&ok, 16);
+    // Convert the steps to degree
+    return dec_steps * total_tilt_angle / static_cast<double> (number_tilt_steps);
+  }
+
   /* Camera management */
   // Private member regarding camera shot
   QUrl url_request_one_shot;
 
+  // DEFINE IN THE CPP FILE
+  // Private member about the camera zoom
+  // The input can be:
+  //   - optical zoom - oz-{1 ... 26}
+  //   - digital zoom - dz-{1 ... 12}
+  static const std::map< QString, QString > zoom_position_key_to_hex;
+  // Declare the opposit search
+  static const std::map< QString, QString > zoom_position_hex_to_key;
+
+  // Private member about the camera focus
+  // The input can be:
+  //   - focus - f-{inf ... 35} in mm
+  static const std::map< QString, QString > focus_position_key_to_hex;
+  // Declare the opposit search
+  static const std::map< QString, QString > focus_position_hex_to_key;
+
 public slots:
   // slot to take decision about the transmitted data
   void net_data_transmitted(QNetworkReply* _p_net_reply);
-
-
-
-  // Relative motion
-  
-
-  //     QString CamAddress;
-  //     QString URLCommand;
-  //     QString URLDownload;
-  //     QString fileName;
-
-  //     QUrl url;
-  //     QUrl urlD;
-  //     QNetworkAccessManager qnam;
-  //     QNetworkReply *reply;
-  //     QFile *file;
-
-  // public:
-  //     /// VARIABLES
-
-  //     static char hexVal[16];
-
-  //     static QString pan;
-  //     static QString tilt;
-  //     static int i;
-
-  //     bool downloadFinished;
-
-  //     /// COMMON METHODS
-  //     void startRequests(QUrl url);
-  //     void startRequestDownload(QUrl _url);
-
-  //     /// Functions & Relatives
-  //     void relativeConf(int _rpan, int _rtilt, int _rspeed);
-  //     void absoluteConf(int _apan, int _atilt, int _aspeed);
-  //     QString convToHexPan(int _decP);
-  //     QString convToHexTilt(int _decT);
-
-  //     //delay
-  //     void delay(int _delay);
-
-// signals:
-
-// public slots:
-       // void downloadFile(QString fname);
-       // void httpReadyRead();
-       // void httpFinished();
-
 };
 
 #endif  // SONYSNCRX550N_H_
